2ee4917120e27bf3d35786bd0ad324e70e954447
frankmolcas92 <37858279+Aka-DoctorCode@users.noreply.github.com>
2023-04-25 19:23:42 -0400

    coloeres

--- Portafolio/Proyecto React/pokedex/src/App.js
+++ Portafolio/Proyecto React/pokedex/src/App.js
@@ -5,7 +5,7 @@ import Alo from './Fondos/1.png'
 
 function App() {
   // Cambio de velocidad del Alo
-  const [AloF,setAloF] = useState("Alo");
+  const [AloSpeed,setAloSpeed] = useState("Alo");
   // funcione modificadoras de estilo
   const [Pokedex,setPokedex] = useState("CLICK TO ACTIVATE POKEDEX")
   const [Puntos,setPuntos] = useState("")
@@ -14,24 +14,24 @@ function App() {
   // funcion para cambiar de componenete
   const navigate = useNavigate();
   //  Cambia estilos de Alo a Alo2
-  function cambiarVelocidad() {
-    setAloF("Alo2")
+  function activatePokedexLoading() {
+    setAloSpeed("Alo2")
     setPokedex("LOADING POKEDEX")
     setPuntos(". . .")
     setCarga("setCarga")
+    // Funcion para cambio de texto a posterior
+    setTimeout(()=>{setPokedex("DATA LOADED");},9200)
     // Funcion para retardar el tiempo de carga
-    setTimeout(
-      ()=>{
-        navigate("/Lista");
-      },10000 
-    )
-  };
+    setTimeout(()=>{navigate("/Lista");},10000)
+    };
+  
 
   return (
     <div className='Main'>
-      <img className={AloF} src={Alo} alt='Alo'/>
-      <button className="Centro" onClick={cambiarVelocidad}>{Pokedex}
-      <p className={Carga}>{Puntos}</p>
+      <img className={AloSpeed} src={Alo} alt='Alo'/>
+      <button className="Centro" onClick={activatePokedexLoading}>
+        <p id='Loading'>{Pokedex}</p>
+        <p className={Carga}>{Puntos}</p>
       </button>
     </div>
   );
--- Portafolio/Proyecto React/pokedex/src/componentes/Lista.jsx
+++ Portafolio/Proyecto React/pokedex/src/componentes/Lista.jsx
@@ -1,20 +1,60 @@
-const Lista = () => {
-    return ( 
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "../TypeColor.css"
+
+const ListaPokeDex = () => {
+    const [pokemon, setPokemon] = useState([]);
+    const [nombres, setNombres] = useState([]);
+
+    async function obtenerDatos() {
+        try {
+            const res = await axios.get(
+            "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=183"
+            );
+        setPokemon(res.data.results);
+        } catch (err) {
+            console.log(`Ocurrio un error: ${err}`);
+        }
+    }
+
+    useEffect(() => {
+        async function obtenerNombres() {
+            const results = await Promise.all(
+                pokemon.map(async (pokemon) => {
+                    try {
+                        const res = await axios.get(
+                        `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+                        );
+                    return res.data;
+                    } catch (err) {
+                        console.log(`Ocurrio un error: ${err}`);
+                    }
+                })
+            );
+            setNombres(results);
+        }
+        obtenerDatos();
+        obtenerNombres();
+    }, [pokemon]);
+
+    return (
         <main>
-            <div>
-                <div>
-                    <p>ID</p>
-                    <p>NAME</p>
-                    <p>TYPES</p>
+            <div className="">
+                {nombres.slice(0, 183).map((element) => (
+                <div className="" key={element.id}>
+                    <p>{element.id}</p>
+                    <p>{element.name}</p>
+                    <img src={element.sprites.front_default} alt="SPRITE" />
+                    <div>
+                    {element.types.map((type) => (
+                        <p className={`${type.type.name}`} key={type.slot}>{`${type.type.name}`}</p>
+                    ))}
+                    </div>
                 </div>
-                <img src="" alt="Sprite" srcset="" />
-                <div>
-                    <button>PREVIOUS</button>
-                    <button>NEXT</button>
-                </div>
+                ))}
             </div>
         </main>
     );
-}
+};
 
-export default Lista;
+export default ListaPokeDex;
