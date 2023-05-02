7be3a1c516f251c776373074f0870cc4d8cf3cf2
frankmolcas92 <37858279+Aka-DoctorCode@users.noreply.github.com>
2023-05-01 21:18:49 -0400

    busqueda

--- Portafolio/Proyecto React/pokedex/src/componentes/Busqueda.jsx
+++ Portafolio/Proyecto React/pokedex/src/componentes/Busqueda.jsx
@@ -1,12 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 const Busqueda = () => {
+    const [pokemon, setPokemon] = useState({});
+    const [nombrePokemon, setNombrePokemon] = useState("pikachu");
+
+    
+    useEffect(() => {
+        function obtenerNombres () {
+          axios.get(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`) 
+          .then((res)=> {
+            setPokemon(res.data)
+            console.log(res.data)
+          })
+          .catch((error) => {  
+            console.log(error)
+          })
+        }
+        obtenerNombres();
+      }, [nombrePokemon]);
+
     return ( 
-        <main className="mt-10">
+        <main className="w-screen h-screen pt-12 flex flex-col justify-center content-center">
             <h1>Serach for a Pokemon</h1>
             <p>input pokemon's name or number</p>
             <div>
-                <input type="Name or Number" />
-                <button>Buscar</button>
+                <input id="nombre" type="text" />
+                <button onClick={
+                    ()=>{
+                        let nombrePokemon = document.getElementById("nombre").value;
+                        console.log(nombrePokemon);
+                        setNombrePokemon(nombrePokemon);
+                        
+                    }
+                }>Buscar</button>
             </div>
+            <p>{pokemon.name}</p>
         </main>
     );
 }
