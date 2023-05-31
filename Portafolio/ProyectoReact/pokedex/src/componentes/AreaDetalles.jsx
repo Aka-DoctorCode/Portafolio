4b2bca52a5efc52a13316ef5e5312bf49e5f49e3
frankmolcas92 <37858279+Aka-DoctorCode@users.noreply.github.com>
2023-05-31 01:36:40 -0400

    update

--- Portafolio/ProyectoReact/pokedex/src/componentes/AreaDetalles.jsx
+++ Portafolio/ProyectoReact/pokedex/src/componentes/AreaDetalles.jsx
@@ -1,15 +1,29 @@
+import React, { useState } from "react";
 import '../Style/AreaDetalles.css'
 // import "../Style/TypeColor.css"
+
+
 const AreaDetalles = (props) => {
+    const [Normal, setNormal] = useState(true);
+    const hadleClickNormal = () => {setNormal(true);};
+    const hadleClickShiny = () => {setNormal(false);};
     return (
         <div className={`${props.types}detalles`}>
-            <div id="cabecera">
-                <p>#&nbsp;&nbsp;{props.id}.</p>
-                <p>{props.name}</p>
-            </div>
-            <div>
-                <img src={props.sprite} className="Sprites" alt="" srcset="" />
-                <img src={props.shinySprite} className="Sprites" alt="" srcset="" />
+            <div id="AreaSuperior">
+                <div id="cabecera">
+                    <p>#&nbsp;&nbsp;{props.id}.</p>
+                    <p>{props.name}</p>
+                </div>
+                <div id="imagenes">
+                    <div>
+                        <button onClick={hadleClickNormal}>Normal</button>
+                        <button onClick={hadleClickShiny}>Shiny</button>
+                    </div>
+                    {Normal 
+                        ? (<img id='Normal' src={props.sprite} className="Sprites" alt="" srcset="" />)
+                        : (<img id='Shiny' src={props.shinySprite} className="Sprites" alt="" srcset="" />)
+                    }
+                </div>
             </div>
             <div>
                 <p className={props.types}>{props.types}</p>
